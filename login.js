40421195e5bbf8c55d9218f6809c5757a0d903d3
mukesh <mb707428@gmail.com>
2026-01-02 11:05:51 +0530

    demo

--- login.js
+++ login.js
@@ -1,4 +1,9 @@
 // auth.js
+let password = "1234"; // ❌ hardcoded secret
+
+function login(input) {
+  return input === password;
+}  
 function login(password) {
   const SECRET = "1234"; // ❌ hardcoded secret
   return password === SECRET;
