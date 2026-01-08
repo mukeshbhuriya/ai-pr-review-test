d49d80704202324dbb226decf11d439686614be5
mukesh <mb707428@gmail.com>
2026-01-08 10:11:48 +0530

    user role

--- validate.js
+++ validate.js
@@ -1,8 +1,12 @@
+const { use } = require("react");
+
 // validate.js
 function isAdmin(user) {
   if ((user.role = "admin")) {
     // ❌ assignment instead of comparison
     return true;
+  } else if (user.role === "user") {
+    return false;
   }
   return false;
 }
