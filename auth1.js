a9f1ec700121bdb11b36a15c5e14b4d8eb2e0c04
mukesh <mb707428@gmail.com>
2026-01-13 15:01:24 +0530

    update

--- auth1.js
+++ auth1.js
@@ -1,7 +1,7 @@
 // auth.js
 
 function login(username, password) {
-  const ADMIN_PASSWORD = "1234"; // ❌ hardcoded password
+  const ADMIN_PASSWORD = "1234"; // ❌ hardcoded
 
   if (password === ADMIN_PASSWORD) {
     return "Login successful";
